169c9ec9ebad704d537f9f39707655cb6ba585cd
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-05-30 14:47:44 -0300

    Finalizando o projeto

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -1,15 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import Stock from '../shared/models/stock-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardService {
 
+  readonly baseUrl = 'http://project-bootcamp-dio-santander.herokuapp.com/bootcamp';
+
   constructor(private http: HttpClient) { }
 
-  async getStocks(){
-    
+  async getStocks(): Promise<Stock[]>{
+    return this.http.get<Stock[]>(`${this.baseUrl}/stock`).toPromise();
   }
 }
 
